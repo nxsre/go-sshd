c9dcc6277412392692341b28544601e42a5102af
wangyj <wangyj@liepin.com>
2017-11-26 10:58:58 +0800

    bug fix

--- server/scp_sinkmode.go
+++ server/scp_sinkmode.go
@@ -29,193 +29,6 @@ type controlMessage struct {
 	atime   int64
 }
 
-// TODO: Cleanup, send errors on protocol errors
-func receiveControlMsg(s ssh.Session) (controlMessage, error) {
-	ctrlmsg := controlMessage{}
-
-	/* 	rs := bufio.NewReader(s)
-	   	ctrlmsgbuf, isprefix, err := rs.ReadLine()
-	   	log.Println("ctrlmsgbuf::", ctrlmsgbuf, string(ctrlmsgbuf), isprefix, err)
-	   	if err == io.EOF {
-	   		return ctrlmsg, nil
-	   	}
-	   	ctrlmsg.msgType = string(ctrlmsgbuf[0])
-	   	ctrlmsglist := strings.Split(string(ctrlmsgbuf), " ") */
-	// ctrlmsgbuf := make([]byte, 256)
-	// nread, err := s.Read(ctrlmsgbuf)
-	bufios := bufio.NewReader(s)
-	ctrlmsgbuf, err := bufios.ReadSlice('\n')
-	if err != nil {
-		// TODO: Maybe only return it upstream if it's an EOF, otherwise handle it?
-		return ctrlmsg, err
-	}
-	nread := len(ctrlmsgbuf)
-	ctrlmsg.msgType = string(ctrlmsgbuf[0])
-
-	ctrlmsglist := strings.Split(string(ctrlmsgbuf[:nread]), " ")
-	nextctrlmsglist := []string{}
-
-	log.Printf("ctrlmsglist_aa: %v", ctrlmsglist)
-
-	if len(ctrlmsglist) > 3 {
-		nextctrlmsglist = ctrlmsglist[3:]
-
-		ctrlmsglist = ctrlmsglist[:3]
-	}
-	log.Printf("ctrlmsglist: %v", ctrlmsglist)
-
-	// Make sure control message is valid
-	switch string(ctrlmsgbuf[0]) {
-	case "E":
-		fmt.Println(string(ctrlmsgbuf), ctrlmsgbuf)
-		if nread > 2 {
-			// TODO: Protocol error
-			ctrls := strings.Split(string(ctrlmsgbuf[:nread]), "\n")
-			for _, ctrl := range ctrls {
-
-				log.Println([]byte(ctrl), nread)
-
-				if len(ctrl) > 1 {
-					log.Printf("Protocol error, got: %v", string(ctrlmsgbuf[:nread]))
-					return ctrlmsg, errors.New("Protocol error")
-				}
-				err := sendSCPBinaryOK(s)
-				log.Println(err)
-			}
-		}
-		/* 	if len(ctrlmsgbuf) > 2 {
-			log.Printf("Protocol error, got: %v", string(ctrlmsgbuf))
-			return ctrlmsg, errors.New("Protocol error")
-		} */
-		return ctrlmsg, nil
-	case "C":
-		if len(ctrlmsglist) != 3 {
-			log.Println("len(ctrlmsglist) != 3")
-			return ctrlmsg, errors.New("ctrlmsglist illegal,Protocol error")
-		}
-
-		ctrlmsg.name = ctrlmsglist[2][:len(ctrlmsglist[2])-1] // Remove trailing newline
-
-		// ctrlmsg.name = ctrlmsglist[2]
-		size, err := strconv.ParseInt(ctrlmsglist[1], 10, 64)
-		ctrlmsg.size = uint64(size)
-		if err != nil {
-			return ctrlmsg, errors.New("Protocol error")
-		}
-
-		fmt.Println("ctrlmsglist[0][1:]", ctrlmsglist[0][1:])
-		mode, err := strconv.ParseInt(ctrlmsglist[0][1:], 8, 32)
-		ctrlmsg.mode = os.FileMode(mode)
-		if err != nil {
-			return ctrlmsg, errors.New("Protocol error")
-		}
-		sendSCPBinaryOK(s)
-	case "D":
-		if len(ctrlmsglist) != 3 {
-			log.Println("len(ctrlmsglist) != 3")
-			return ctrlmsg, errors.New("ctrlmsglist illegal,Protocol error")
-		}
-
-		ctrlmsg.name = ctrlmsglist[2][:len(ctrlmsglist[2])-1] // Remove trailing newline
-		// ctrlmsg.name = ctrlmsglist[2]
-		size, err := strconv.ParseInt(ctrlmsglist[1], 10, 64)
-		ctrlmsg.size = uint64(size)
-		if err != nil {
-			return ctrlmsg, errors.New("Protocol error")
-		}
-
-		fmt.Println("ctrlmsglist[0][1:]", ctrlmsglist[0][1:])
-		mode, err := strconv.ParseInt(ctrlmsglist[0][1:], 8, 32)
-		ctrlmsg.mode = os.FileMode(mode)
-		if err != nil {
-			return ctrlmsg, errors.New("Protocol error")
-		}
-		sendSCPBinaryOK(s)
-
-	case "T":
-		ctrlmsg.mtime, err = strconv.ParseInt(ctrlmsglist[0][1:], 10, 64)
-		if err != nil {
-			return ctrlmsg, errors.New("mtime.sec not delimited")
-		}
-		ctrlmsg.atime, err = strconv.ParseInt(ctrlmsglist[2], 10, 64)
-		if err != nil {
-			return ctrlmsg, errors.New("atime.sec not delimited")
-		}
-		sendSCPBinaryOK(s)
-
-		// A "T" message will always come before a "D" or "C", so we can combine both
-		newCtrlmsg := controlMessage{}
-		log.Println("444444444444", len(nextctrlmsglist), nextctrlmsglist)
-		if len(nextctrlmsglist) >= 3 {
-			ctrlmsglist = nextctrlmsglist[:3]
-			fmt.Println("#############", ctrlmsglist)
-			mode, err := strconv.ParseInt(ctrlmsglist[0][3:], 8, 32)
-			if err != nil {
-				return newCtrlmsg, errors.New("1111 Protocol error")
-			}
-
-			log.Println("ctrlmsglist[1]", ctrlmsglist[1])
-			size, err := strconv.ParseUint(ctrlmsglist[1], 10, 64)
-			if err != nil {
-				log.Println(err)
-				return newCtrlmsg, errors.New("22222 Protocol error")
-			}
-			newCtrlmsg = controlMessage{
-				msgType: string(ctrlmsglist[0][2]),
-				mode:    os.FileMode(mode),
-				size:    size,
-				name:    strings.TrimRight(ctrlmsglist[2], "\n"),
-			}
-		} else {
-			newCtrlmsg, err = receiveControlMsg(s)
-			if err != nil {
-				log.Println("newCtrlmsg error:", err)
-				return ctrlmsg, errors.New("newCtrlmsg error,Protocol error")
-			}
-
-		}
-		sendSCPBinaryOK(s)
-
-		newCtrlmsg.mtime = ctrlmsg.mtime
-		newCtrlmsg.atime = ctrlmsg.atime
-		log.Println("AAAAAA:", newCtrlmsg, ctrlmsg)
-
-		return newCtrlmsg, nil
-
-	default:
-		// TODO: We have an expected message here, report it and abort
-	}
-
-	// if ctrlmsg.msgType == "T" {
-
-	// }
-
-	/* 	if len(ctrlmsglist) != 3 {
-	   		log.Println("len(ctrlmsglist) != 3")
-	   		return ctrlmsg, errors.New("ctrlmsglist illegal,Protocol error")
-	   	}
-
-	   	ctrlmsg.name = ctrlmsglist[2][:len(ctrlmsglist[2])-1] // Remove trailing newline
-	   	// ctrlmsg.name = ctrlmsglist[2]
-	   	size, err := strconv.ParseInt(ctrlmsglist[1], 10, 64)
-	   	ctrlmsg.size = uint64(size)
-	   	if err != nil {
-	   		debug.PrintStack()
-	   		return ctrlmsg, errors.New("Protocol error")
-	   	}
-
-	   	fmt.Println("ctrlmsglist[0][1:]", ctrlmsglist[0][1:])
-	   	mode, err := strconv.ParseInt(ctrlmsglist[0][1:], 8, 32)
-	   	ctrlmsg.mode = os.FileMode(mode)
-	   	if err != nil {
-	   		debug.PrintStack()
-
-	   		return ctrlmsg, errors.New("Protocol error")
-	   	}
-	   	sendSCPBinaryOK(s) */
-	return ctrlmsg, nil
-}
-
 // Generate a full path out of our basedir, the directories currently in the stack, and the target
 func (config scpConfig) generatePath(dirStack []string, target string) string {
 	fmt.Println("DIRSTACK:", dirStack)
@@ -270,7 +83,6 @@ func (c scpConfig) receiveFileContents(s ssh.Session, dirStack []string, msgctrl
 			return err
 		}
 	}
-	sendSCPBinaryOK(s)
 
 	statusbuf := make([]byte, 1)
 	_, err = s.Read(statusbuf)
@@ -278,6 +90,7 @@ func (c scpConfig) receiveFileContents(s ssh.Session, dirStack []string, msgctrl
 		log.Printf("Getting status error after transfer: %v", err)
 		return err
 	}
+	fmt.Println("statusbuf:::", statusbuf)
 	sendSCPBinaryOK(s)
 	return err
 }
@@ -348,12 +161,20 @@ func (config scpConfig) startSCPSink(s ssh.Session, opts scpOptions) error {
 
 	// Tell the other side we're ready to start receiving data
 	sendSCPBinaryOK(s)
-	i := 1
 	for {
-		fmt.Println(i)
-		i++
-		ctrlmsg, err := receiveControlMsg(s)
+		// Steps here:
+		// 1. Figure out what kind of message this is (file, directory, time, end of directory...)
+		// If it's D:
+		//   - Add a new directory to the stack
+		//   - Create the directory
+		// If it's E:
+		//   - Remove one directory from the stack
+		// If it's C:
+		//   - Receive file and store it in the current stack
+		// If it's T:
+		//   - Receive the next control message
 
+		ctrlmsg, err := receiveControlMsg(s)
 		if err != nil {
 			if err == io.EOF {
 				// EOF is fine at this point, it just means no more files to copy
@@ -362,9 +183,6 @@ func (config scpConfig) startSCPSink(s ssh.Session, opts scpOptions) error {
 			log.Printf("Got error from client: %v", err)
 			break
 		}
-
-		log.Printf("Message type: %v", ctrlmsg.msgType)
-		log.Println("Message: ", ctrlmsg, dirStack)
 		switch ctrlmsg.msgType {
 		case "D":
 			log.Println("创建目录", dirStack, ctrlmsg.name, opts.fileNames)
@@ -378,6 +196,7 @@ func (config scpConfig) startSCPSink(s ssh.Session, opts scpOptions) error {
 				dir, file := filepath.Split(target)
 				target = file
 				dirStack = append(dirStack, dir, file)
+				log.Printf("target dir stack is now: %v", dirStack)
 			} else {
 				// TODO: Figure out how we need to behave in terms of permissions/times, etc
 				err := createDir(config.generatePath(dirStack, ctrlmsg.name), ctrlmsg)
@@ -385,8 +204,8 @@ func (config scpConfig) startSCPSink(s ssh.Session, opts scpOptions) error {
 					return err
 				}
 				dirStack = append(dirStack, ctrlmsg.name)
+				log.Printf("wwwwww dir stack is now: %v", dirStack)
 			}
-			log.Printf("dir stack is now: %v", dirStack)
 		case "E":
 			stackSize := len(dirStack)
 			if (opts.TargetIsDir && stackSize <= 1) || (!opts.TargetIsDir && stackSize <= 0) {
@@ -395,7 +214,6 @@ func (config scpConfig) startSCPSink(s ssh.Session, opts scpOptions) error {
 				return errors.New(msg)
 			}
 			dirStack = dirStack[:len(dirStack)-1]
-			fmt.Println("66666666666666666666")
 		case "C":
 			var filename string
 			if opts.TargetIsDir {
@@ -412,18 +230,85 @@ func (config scpConfig) startSCPSink(s ssh.Session, opts scpOptions) error {
 			}
 		}
 
-		// Steps here:
-		// 1. Figure out what kind of message this is (file, directory, time, end of directory...)
-		// If it's D:
-		//   - Add a new directory to the stack
-		//   - Create the directory
-		// If it's E:
-		//   - Remove one directory from the stack
-		// If it's C:
-		//   - Receive file and store it in the current stack
-		// If it's T:
-		//   - Receive the next control message
-
 	}
 	return nil
+}
+
+// 每次读取一行头控制信息
+func receiveControlMsg(s ssh.Session) (controlMessage, error) {
+	ctrlmsg := controlMessage{}
+	bufios := bufio.NewReader(s)
+	ctrlmsgbuf, isp, err := bufios.ReadLine()
+	fmt.Println(ctrlmsgbuf, isp, err)
+	if err != nil {
+		// TODO: Maybe only return it upstream if it's an EOF, otherwise handle it?
+		return ctrlmsg, err
+	}
+	log.Println("ctrlmsgbuf:", ctrlmsgbuf)
+	ctrlmsg.msgType = string(ctrlmsgbuf[0])
+
+	ctrlmsglist := strings.Split(string(ctrlmsgbuf), " ")
+	log.Printf("控制信息列表: %v", ctrlmsglist)
+
+	if len(ctrlmsglist) > 3 {
+		ctrlmsglist = ctrlmsglist[:3]
+	}
+	log.Printf("前三段有效控制信息: %v", ctrlmsglist)
+
+	// Make sure control message is valid
+	switch string(ctrlmsg.msgType) {
+	case "E":
+		sendSCPBinaryOK(s)
+		if len(ctrlmsgbuf) > 2 {
+			log.Printf("Protocol error, got: %v", string(ctrlmsgbuf))
+			return ctrlmsg, errors.New("Protocol error")
+		}
+		return ctrlmsg, nil
+	case "C":
+	case "D":
+	case "T":
+		ctrlmsg.mtime, err = strconv.ParseInt(ctrlmsglist[0][1:], 10, 64)
+		if err != nil {
+			return ctrlmsg, errors.New("mtime.sec not delimited")
+		}
+		ctrlmsg.atime, err = strconv.ParseInt(ctrlmsglist[2], 10, 64)
+		if err != nil {
+			return ctrlmsg, errors.New("atime.sec not delimited")
+		}
+		sendSCPBinaryOK(s)
+		// A "T" message will always come before a "D" or "C", so we can combine both
+		newCtrlmsg := controlMessage{}
+		newCtrlmsg, err = receiveControlMsg(s)
+		if err != nil {
+			log.Println("newCtrlmsg error:", err)
+			return ctrlmsg, errors.New("newCtrlmsg error,Protocol error")
+		}
+		newCtrlmsg.mtime = ctrlmsg.mtime
+		newCtrlmsg.atime = ctrlmsg.atime
+		log.Println("AAAAAA:", newCtrlmsg, ctrlmsg)
+
+		return newCtrlmsg, nil
+	default:
+		// TODO: We have an expected message here, report it and abort
+	}
+
+	if len(ctrlmsglist) != 3 {
+		log.Println("len(ctrlmsglist) != 3")
+		return ctrlmsg, errors.New("ctrlmsglist illegal,Protocol error")
+	}
+
+	ctrlmsg.name = ctrlmsglist[2] // Remove trailing newline
+	size, err := strconv.ParseInt(ctrlmsglist[1], 10, 64)
+	ctrlmsg.size = uint64(size)
+	if err != nil {
+		return ctrlmsg, errors.New("Protocol error")
+	}
+
+	mode, err := strconv.ParseInt(ctrlmsglist[0][1:], 8, 32)
+	ctrlmsg.mode = os.FileMode(mode)
+	if err != nil {
+		return ctrlmsg, errors.New("Protocol error")
+	}
+	sendSCPBinaryOK(s)
+	return ctrlmsg, nil
 }
